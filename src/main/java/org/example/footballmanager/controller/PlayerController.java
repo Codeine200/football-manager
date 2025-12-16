f2b8a0c8180492568c4d48217d43a3b6fab967f6
v.holovko <codeine200@gmail.com>
2025-12-17 01:21:12 +0200

    add http requests

--- src/main/java/org/example/footballmanager/controller/PlayerController.java
+++ src/main/java/org/example/footballmanager/controller/PlayerController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-@RequestMapping("/api/players")
+@RequestMapping("/api/v1/players")
 @RequiredArgsConstructor
 public class PlayerController {
 
